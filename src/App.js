29f3b97c638912a04c82ac48a7c7ba40a4a16951
indre101 <zygaityte.indre@gmail.com>
2020-05-12 19:51:13 +0200

    displayes beers

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import DataBase from "./modules/DataBase";
 import "./App.scss";
 
 function App() {
-  return <div className="App">hi</div>;
+  const [beers, setBeers] = useState([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      const Beers = await DataBase();
+      setBeers(Beers);
+    };
+
+    getData();
+  }, []);
+
+  const test = beers.map((beer) => <div key={beer.name}>{beer.name}</div>);
+  return <div className="App">{test}</div>;
 }
 
 export default App;
