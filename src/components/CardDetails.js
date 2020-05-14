77c70508480bb834ce456275f21b2b5cf0f32f34
indre101 <zygaityte.indre@gmail.com>
2020-05-14 20:08:18 +0200

    adds basic card payment

---
+++ src/components/CardDetails.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { PaymentInputsWrapper, usePaymentInputs } from "react-payment-inputs";
+import images from "react-payment-inputs/images";
+
+export default function CardDetails() {
+  const {
+    wrapperProps,
+    getCardImageProps,
+    getCardNumberProps,
+    getExpiryDateProps,
+    getCVCProps,
+  } = usePaymentInputs();
+
+  return (
+    <div>
+      <PaymentInputsWrapper {...wrapperProps}>
+        <svg {...getCardImageProps({ images })} />
+        <input {...getCardNumberProps()} />
+        <input {...getExpiryDateProps()} />
+        <input {...getCVCProps()} />
+      </PaymentInputsWrapper>
+    </div>
+  );
+}
