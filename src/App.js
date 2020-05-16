66eddb00f403eaca6eadd886fa40e455e57e0f6f
Graeme Clark <grae0026@stud.kea.dk>
2020-05-16 10:23:32 +0200

    update sass partials

--- src/App.js
+++ src/App.js
@@ -3,19 +3,19 @@ import DataBase from "./modules/DataBase";
 import "./App.scss";
 
 function App() {
-  const [beers, setBeers] = useState([]);
+	const [beers, setBeers] = useState([]);
 
-  useEffect(() => {
-    const getData = async () => {
-      const Beers = await DataBase.GetData();
-      setBeers(Beers);
-    };
+	useEffect(() => {
+		const getData = async () => {
+			const Beers = await DataBase.GetData();
+			setBeers(Beers);
+		};
 
-    getData();
-  }, []);
+		getData();
+	}, []);
 
-  const test = beers.map((beer) => <div key={beer.name}>{beer.name}</div>);
-  return <div className="App">{test}</div>;
+	const test = beers.map((beer) => <div key={beer.name}>{beer.name}</div>);
+	return <div className="App">{test}</div>;
 }
 
 export default App;
