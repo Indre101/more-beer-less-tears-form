3aacbb9236b9b4cd3867d6a2ea76beaf7259670d
indre101 <zygaityte.indre@gmail.com>
2020-05-21 11:12:35 +0200

    adds beer card info

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -5,56 +5,73 @@ import { Link } from "react-router-dom";
 import imageTest from "../assets/images/row26.png";
 
 function Shop() {
-	const [beers, setBeers] = useState([]);
-	const [beersOnTap, setBeersOnTap] = useState([]);
+  const [beers, setBeers] = useState([]);
+  const [beersOnTap, setBeersOnTap] = useState([]);
+  const [beersAvailableTobuy, setbeersAvailableTobuy] = useState([]);
 
-	// get beer descriptions
-	useEffect(() => {
-		const getData = async () => {
-			const Beers = await DataBase.GetData();
-			setBeers(Beers);
-			// console.log(Beers);
-		};
-		getData(beers);
-	}, []);
+  // get beers on tap
+  useEffect(() => {
+    const getData = async () => {
+      const Beers = await DataBase.GetData();
+      setBeersOnTap(Beers.taps);
+    };
+    getData(beers);
+    return;
+  }, []); // eslint-disable-line
 
-	// get all bar data - and filter taps
-	useEffect(() => {
-		const getData = async () => {
-			const barData = await DataBase.GetData();
-			setBeersOnTap(barData.taps);
-			console.log(barData);
-		};
-		getData(beersOnTap);
-	}, []);
+  // get all beer data
+  useEffect(() => {
+    const getData = async () => {
+      const barData = await DataBase.GetBeerTypes();
+      setBeers(barData);
+    };
+    getData(beersOnTap);
+    return;
+  }, []); // eslint-disable-line
 
-	return (
-		<div className="main-wrapper">
-			<div className="page-title">
-				<h1>Shop</h1>
-			</div>
-			<section className="beer-list">
-				{beersOnTap.map((beer) => (
-					<div className="single-beer" key={beer.id}>
-						<img src={imageTest} />
-						{/* <img src={`static/${beer.label}`} /> */}
-						<h2 key={beer.beer}>{beer.beer}</h2>
+  useEffect(() => {
+    const uniqueBeerNamesOnTap = [
+      ...new Set(beersOnTap.map((beerName) => beerName.beer)),
+    ];
 
-						<Link to={`/shop/${beer.id}`}>
-							<button>More Info</button>
-						</Link>
-						<button>Add to cart</button>
-					</div>
-				))}
-			</section>
-			<section className="promo-section">
-				<h1>Happy Hour</h1>
-			</section>
-			<section className="stats-section">
-				<h1>Stats</h1>
-			</section>
-		</div>
-	);
+    const beersAvailableToBuyFiltered = uniqueBeerNamesOnTap.map((beerName) => {
+      const beerObj = beers.find((type) => {
+        return type.name === beerName;
+      });
+      return beerObj;
+    });
+    setbeersAvailableTobuy(beersAvailableToBuyFiltered);
+  }, [beers, beersOnTap]);
+
+  console.log(beersAvailableTobuy);
+
+  const beersAvailableTobuyElement = beersAvailableTobuy.map((beer) => (
+    <div className="single-beer" key={beer && beer.name}>
+      <img src={require(`../assets/images/${beer.label}`)} alt="Beer" />
+      {/* <img src={`static/${beer.label}`} /> */}
+      <h2 key={beer.beer}>{beer.beer}</h2>
+
+      <Link to={`/shop/${beer.id}`}>
+        <button>More Info</button>
+      </Link>
+      <button>Add to cart</button>
+    </div>
+  ));
+
+  return (
+    <div className="main-wrapper">
+      <div className="page-title">
+        <h1>Shop</h1>
+      </div>
+      <section className="beer-list">{beersAvailableTobuyElement}</section>
+      <section className="promo-section">
+        <h1>Happy Hour</h1>
+      </section>
+      <section className="stats-section">
+        <h1>Stats</h1>
+      </section>
+    </div>
+  );
 }
 
 export default Shop;
