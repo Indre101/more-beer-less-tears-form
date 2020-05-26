8e1f434dcfe2a3c66eb7e57ac508809889636a37
indre101 <zygaityte.indre@gmail.com>
2020-05-26 11:57:35 +0200

    adds two options for payment

--- src/components/Payment.js
+++ src/components/Payment.js
@@ -1,10 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function Payment(props) {
+  const [paymentMethod, setpaymentMethod] = useState();
+
+  function handleChange(event) {
+    const { value } = event.target;
+    setpaymentMethod(value);
+  }
+
   return (
     <div>
-      Payment
+      <form className="optionsForPayment">
+        <label htmlFor="mobpay">Mobile Pay</label>
+        <input
+          id="mobpay"
+          type="radio"
+          name="payment"
+          value="Mobile Pay"
+          checked={paymentMethod === "Mobile Pay"}
+          onChange={handleChange}
+        />
+        <label htmlFor="card">Card Payment</label>
+        <input
+          id="card"
+          type="radio"
+          name="payment"
+          value="Card payment"
+          checked={paymentMethod === "Card payment"}
+          onChange={handleChange}
+        />
+      </form>
+
       <Link to="/confirmation">
         <input type="button" value="Next" />
       </Link>
