58ded73bfbe388ea1b028cf4c81a3dd95046e6c8
Graeme Clark <grae0026@stud.kea.dk>
2020-05-28 15:41:27 +0200

    move cloud to header and fix animation

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,32 @@
 import React, { useState, useEffect } from "react";
 import "../App.scss";
+import gsap from "gsap";
 
 function Header() {
+	useEffect(() => {
+		// var tl = gsap.timeline({ delay: 0 });
+		// tl.from(".animFoam", { opacity: 0, y: 40, duration: 3 });
+		// tl.to(".animFoam", { opacity: 1, y: 0 });
+		gsap.from(".animFoam", { duration: 1, y: 30, opacity: 0, stagger: 0.8 });
+	});
 	return (
 		<div className="site-header">
-			<div className="logo-container">
+			<div className="header-background animFoam">
+				<div className="logo-container">
+					<img
+						className="foobar-logo"
+						src={require("../../src/assets/svg/foobar_logo.svg")}
+						alt="Foobar logo"
+					/>
+				</div>
+			</div>
+			<div className="cloud-top animFoam">
 				<img
-					className="foobar-logo"
-					src={require("../../src/assets/svg/foobar_logo.svg")}
-					alt="Foobar logo"
+					className="cloud"
+					src={require("../../src/assets/svg/cloud.svg")}
+					alt="cloud"
 				/>
 			</div>
-			{/* <div className="title">
-				<h1>Remote Bar</h1>
-			</div> */}
 		</div>
 	);
 }
