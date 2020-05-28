a51e10b3c82291f5579e4788cb2c5ad5dc8086f1
indre101 <zygaityte.indre@gmail.com>
2020-05-28 12:53:09 +0200

    styles mobile pay

--- src/components/MobilePay.js
+++ src/components/MobilePay.js
@@ -1,5 +1,14 @@
 import React from "react";
 
 export default function MobilePay() {
-  return <div>Transfer the payment amount 789809</div>;
+  return (
+    <div className="mobilePay">
+      <div className="amount">
+        <h2>Total amount: 74</h2>
+      </div>
+      <img src={require("../assets/svg/foobar_logo.svg")} alt="Logo" />
+      <h4>Mobile-pay user</h4>
+      <h3>FooBar</h3>
+    </div>
+  );
 }
