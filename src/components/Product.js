c67dd96d7d8a2dc4cee3f393244cbac7414bbc2e
Graeme Clark <grae0026@stud.kea.dk>
2020-05-28 08:49:00 +0200

    merge

--- src/components/Product.js
+++ src/components/Product.js
@@ -5,64 +5,21 @@ import gsap from "gsap";
 import { Button } from "./Buttons";
 
 function Product(props) {
-  //render props
-  // animate gsap elements in
-  useEffect(() => {
-    gsap.from(".animUp", { duration: 1, y: 50, opacity: 1, stagger: 0.5 });
-  });
+	//render props
+	// animate gsap elements in
+	useEffect(() => {
+		gsap.from(".animUp", { duration: 1, y: 50, opacity: 1, stagger: 0.5 });
+	});
 
-  return (
-    <OrderControl
-      beer={props.location.state.beer}
-      props={props}
-      render={(beerCount, setbeerCount, createOrder, beer) => (
-        <div className="main-wrapper">
-          {/* <div className="page-title">
+	return (
+		<OrderControl
+			beer={props.location.state.beer}
+			props={props}
+			render={(beerCount, setbeerCount, createOrder, beer) => (
+				<div className="main-wrapper">
+					{/* <div className="page-title">
             <h1>Product</h1>
           </div> */}
-<<<<<<< HEAD
-          <div className="product-page-layout">
-            <div className="product-image-container animUp">
-              <img
-                className="product-image"
-                src={require(`../assets/images/${beer.label}`)}
-                alt="beer"></img>
-            </div>
-            <section className="product-info animUp">
-              <h1>{beer.name}</h1>
-              <h2>{beer.category}</h2>
-              <blockquote>
-                <p>{beer.description.overallImpression}</p>
-              </blockquote>
-              <h2>{beer.price}kr</h2>
-              <button onClick={() => setbeerCount(beerCount + 1)}>
-                Increment
-              </button>
-              <button
-                onClick={() =>
-                  setbeerCount((prevBeeerCount) =>
-                    prevBeeerCount === 0 ? 0 : prevBeeerCount - 1
-                  )
-                }>
-                Decrement
-              </button>
-              <h2>{beerCount}</h2>
-              <button onClick={createOrder}>Add to cart</button>
-            </section>
-            <section className="product-main-description animUp">
-              <h2>Appearance</h2>
-              <p>{beer.description.appearance}</p>
-              <h2>Flavor</h2>
-              <p>{beer.description.flavor}</p>
-              <h2>Aroma</h2>
-              <p>{beer.description.aroma}</p>
-            </section>
-          </div>
-        </div>
-      )}
-    />
-  );
-=======
 					<div className="product-page-layout">
 						<div className="product-image-container animUp">
 							<img
@@ -114,7 +71,6 @@ function Product(props) {
 			)}
 		/>
 	);
->>>>>>> graeme_third
 }
 
 export default Product;
