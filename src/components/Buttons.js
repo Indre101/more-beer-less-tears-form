7cf9f495bc6584c8dfd837ac64866d48d4d5b7df
indre101 <zygaityte.indre@gmail.com>
2020-05-29 18:43:59 +0200

    finished validation

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -3,19 +3,23 @@ import "../App.scss";
 import gsap from "gsap";
 
 const styles = [
-	"btn--primary--solid",
-	"btn--primary--outline",
-	"btn--secondary--solid",
-	"btn--success--solid",
-	"btn--counter--outline",
+  "btn--primary--solid",
+  "btn--primary--outline",
+  "btn--secondary--solid",
+  "btn--success--solid",
+  "btn--counter--outline",
 ];
 
 export const Button = ({ children, type, onClick, buttonStyle }) => {
-	const checkButtonStyle = styles.includes(buttonStyle) ? buttonStyle : styles[0];
+  const checkButtonStyle = styles.includes(buttonStyle)
+    ? buttonStyle
+    : styles[0];
 
-	return (
-		<button className={`btn ${checkButtonStyle}`} onClick={onClick} type={type}>
-			{children}
-		</button>
-	);
+  return (
+    <div onClick={onClick}>
+      <button className={`btn ${checkButtonStyle}`} type={type}>
+        {children}
+      </button>
+    </div>
+  );
 };
