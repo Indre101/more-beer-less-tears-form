6e508a6d7043fc54f5a06bd872221a23f5570711
Graeme Clark <grae0026@stud.kea.dk>
2020-05-29 11:26:50 +0200

    working on order message

--- src/components/OrderMessage.js
+++ src/components/OrderMessage.js
@@ -4,14 +4,16 @@ import { Button } from "./Buttons";
 
 const OrderMessage = (props) => {
 	return (
-		<div>
+		<div className="wrapper">
 			<div>Your order number is : {props.location.state.orderNumber} </div>
 			<Link
 				to={{
 					pathname: `/shop`,
 				}}
 			>
-				<Button>Go back to the shop</Button>
+				<Button type="button" buttonStyle="btn--secondary--solid">
+					Go to the shop
+				</Button>
 			</Link>
 		</div>
 	);
