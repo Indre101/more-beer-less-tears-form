5dee29dd75466077f9e7f4625562b73ef151bd78
Graeme Clark <grae0026@stud.kea.dk>
2020-05-30 11:24:11 +0200

    add rising clouds on cards

--- src/components/BeerCardShop.js
+++ src/components/BeerCardShop.js
@@ -5,79 +5,75 @@ import gsap from "gsap";
 import { Button } from "./Buttons";
 
 export default function BeerCardShop(props) {
-  // animation for single beers
-  useEffect(() => {
-    const tl = gsap.timeline({ delay: 2 });
-    tl.from(".animBeer", { opacity: 0, y: 50 });
-    tl.to(".animBeer", { opacity: 1, y: 0, duration: 1 });
-    // gsap.from(".animBeer", { duration: 1, y: -50, opacity: 1, stagger: 0.5 });
-  }, [props.beer]);
-  //render props
-  return (
-    <OrderControl
-      beer={props.beer}
-      props={props}
-      render={(beerCount, setbeerCount, createOrder, beer) => (
-        <div className="single-beer" key={beer.id}>
-          <div className="single-beer-image-container">
-            <img
-              className="single-beer-image animBeer"
-              src={require(`../assets/images/${
-                beer.label ? beer.label : "elhefe.png"
-              }`)}
-              alt="Beer"
-            />
-          </div>
-          <div className="single-beer-cloud">
-            <img
-              className="cloud"
-              src={require("../../src/assets/svg/cloud.svg")}
-              alt="cloud"
-            />
-          </div>
-          <div className="single-beer-info">
-            <h2 className="single-beer-title">{beer ? beer.name : " "}</h2>
-            <h3 className="single-beer-category">
-              {beer ? beer.category : " "}
-            </h3>
-            <h3>{beer.price}Kr</h3>
-            <div className="single-beer-counter">
-              <Button
-                onClick={() =>
-                  setbeerCount((prevBeeerCount) =>
-                    prevBeeerCount === 0 ? 0 : prevBeeerCount - 1
-                  )
-                }
-                type="button"
-                buttonStyle="btn--counter--outline">
-                -
-              </Button>
-              <h2>{beerCount}</h2>
-              <Button
-                onClick={() => setbeerCount((prevCount) => prevCount + 1)}
-                type="button"
-                buttonStyle="btn--counter--outline">
-                +
-              </Button>
-            </div>
-            <Link to={{ pathname: `/shop/product`, state: { beer: beer } }}>
-              <Button type="button" buttonStyle="btn--secondary--solid">
-                More Info
-              </Button>
-            </Link>
-            <Button
-              onClick={createOrder}
-              type="button"
-              buttonStyle="btn--primary--solid">
-              Add to cart
-            </Button>
-          </div>
-        </div>
-      )}
-    />
-  );
+	// animation for single beers
+	useEffect(() => {
+		const tl = gsap.timeline({ delay: 0 });
+		tl.from(".animBeer", { opacity: 0, y: 50 });
+		tl.from(".animCloud", { opacity: 0, y: 50 });
+		tl.to(".animBeer", { opacity: 1, y: 0, stagger: 0.5 });
+		tl.to(".animCloud", { opacity: 1, y: 0, stagger: 0.5 });
+	}, [props.beer]);
+	//render props
+	return (
+		<OrderControl
+			beer={props.beer}
+			props={props}
+			render={(beerCount, setbeerCount, createOrder, beer) => (
+				<div className="single-beer" key={beer.id}>
+					<div className="single-beer-image-container">
+						<img
+							className="single-beer-image animBeer"
+							src={require(`../assets/images/${beer.label ? beer.label : "elhefe.png"}`)}
+							alt="Beer"
+						/>
+					</div>
+					<div className="single-beer-cloud">
+						<img
+							className="cloud animCloud"
+							src={require("../../src/assets/svg/cloud.svg")}
+							alt="cloud"
+						/>
+					</div>
+					<div className="single-beer-info">
+						<h2 className="single-beer-title">{beer ? beer.name : " "}</h2>
+						<h3 className="single-beer-category">{beer ? beer.category : " "}</h3>
+						<h3>{beer.price}Kr</h3>
+						<div className="single-beer-counter">
+							<Button
+								onClick={() =>
+									setbeerCount((prevBeeerCount) =>
+										prevBeeerCount === 0 ? 0 : prevBeeerCount - 1
+									)
+								}
+								type="button"
+								buttonStyle="btn--counter--outline"
+							>
+								-
+							</Button>
+							<h2>{beerCount}</h2>
+							<Button
+								onClick={() => setbeerCount((prevCount) => prevCount + 1)}
+								type="button"
+								buttonStyle="btn--counter--outline"
+							>
+								+
+							</Button>
+						</div>
+						<Link to={{ pathname: `/shop/product`, state: { beer: beer } }}>
+							<Button type="button" buttonStyle="btn--secondary--solid">
+								More Info
+							</Button>
+						</Link>
+						<Button onClick={createOrder} type="button" buttonStyle="btn--primary--solid">
+							Add to cart
+						</Button>
+					</div>
+				</div>
+			)}
+		/>
+	);
 }
 
 BeerCardShop.defaultProps = {
-  orders: [],
+	orders: [],
 };
