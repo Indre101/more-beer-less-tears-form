175af91e8ea78e71b0f8c2530ef9c98b137527dc
Graeme Clark <grae0026@stud.kea.dk>
2020-05-31 13:40:47 +0200

    fix max-width on header

--- src/components/Confirmation.js
+++ src/components/Confirmation.js
@@ -1,20 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import DataBase from "../modules/DataBase";
 import { Link } from "react-router-dom";
 import { Button } from "./Buttons";
+import gsap from "gsap";
 
 export default function Confirmation(props) {
 	const { orders, user, paymentMethod, totalAmount } = props.location.state;
 	// const [ordderNumber, setordderNumber] = useState(0);
 
+	useEffect(() => {
+		gsap.to(".order-summary", {
+			duration: 1,
+			y: 0,
+			opacity: 1,
+			stagger: 0.5,
+		});
+	});
+
 	const orderSummary = orders.map((order) => (
 		<div className="order-summary" key={Math.random() * 4000}>
-			<h2>Beers</h2>
-			<h2>{order.name}</h2>
-			<h2>amount</h2>
-			<h2>{order.amount}</h2>
-			<h2>sum</h2>
-			<h2>{order.amount * order.price}</h2>
+			<h4>
+				{order.amount} x {order.name}
+			</h4>
+			<h4>{order.amount * order.price}Kr</h4>
 		</div>
 	));
 
@@ -35,12 +43,24 @@ export default function Confirmation(props) {
 
 	return (
 		<div className="main-wrapper confirmation">
-			<h3>Name{user.name} </h3>
-			<h3>phone{user.phone} </h3>
-			<h3>payment method{paymentMethod}</h3>
-			<div className="ordersumary">{orderSummary}</div>
-			<h3>total{totalAmount}</h3>
-
+			<h1>Order details</h1>
+			<section className="order-summary-personal">
+				<h2>Personal info</h2>
+				<h3>Name</h3>
+				<h4>{user.name}</h4>
+				<h3>Phone</h3>
+				<h4>{user.phone}</h4>
+				<h3>Payment method</h3>
+				<h4>{paymentMethod}</h4>
+			</section>
+			<section className="ordersumary">
+				<h2>Beers</h2>
+				<div>{orderSummary}</div>
+			</section>
+			<section className="order-summary-total">
+				<h2>Total</h2>
+				<h3>{totalAmount}Kr</h3>
+			</section>
 			<div style={{ display: "flex", flexDirection: "column" }}>
 				<Link
 					to={{
