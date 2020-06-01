8b9eab509817a0a0e61798dd32ba9bd10547dcee
indre101 <zygaityte.indre@gmail.com>
2020-06-01 17:41:06 +0200

    Updates

---
+++ precache-manifest.60d8f956c63873ea395f119a80f54b26.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d98fa24f16b05b1f07a791b22d5e88b2",
+    "url": "/more-beer-less-tears-form/index.html"
+  },
+  {
+    "revision": "a16b350932df5687bf56",
+    "url": "/more-beer-less-tears-form/static/css/2.3712154f.chunk.css"
+  },
+  {
+    "revision": "e136c28527afb4637242",
+    "url": "/more-beer-less-tears-form/static/css/main.5c9ed37d.chunk.css"
+  },
+  {
+    "revision": "a16b350932df5687bf56",
+    "url": "/more-beer-less-tears-form/static/js/2.a47155ac.chunk.js"
+  },
+  {
+    "revision": "764c50aac37d1847b78e6a3945cc7fc1",
+    "url": "/more-beer-less-tears-form/static/js/2.a47155ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e136c28527afb4637242",
+    "url": "/more-beer-less-tears-form/static/js/main.b7400b60.chunk.js"
+  },
+  {
+    "revision": "8baf4a83dbea4e391dce",
+    "url": "/more-beer-less-tears-form/static/js/runtime-main.20a1457c.js"
+  },
+  {
+    "revision": "3aef1aca4ebf6812ef07f7bb9f8f19b5",
+    "url": "/more-beer-less-tears-form/static/media/banner.3aef1aca.svg"
+  },
+  {
+    "revision": "ae85458a1caf5c7fe3d19e593a260a7c",
+    "url": "/more-beer-less-tears-form/static/media/bubble.ae85458a.svg"
+  },
+  {
+    "revision": "01349cbe6301fe6e95678e82ccafb0c9",
+    "url": "/more-beer-less-tears-form/static/media/cart.01349cbe.svg"
+  },
+  {
+    "revision": "ff15a75f639e34331100f4bffd5c80a4",
+    "url": "/more-beer-less-tears-form/static/media/cloud.ff15a75f.svg"
+  },
+  {
+    "revision": "d313a8e9b617eb162da3b81ac5ef5d0f",
+    "url": "/more-beer-less-tears-form/static/media/elhefe.d313a8e9.png"
+  },
+  {
+    "revision": "0588f679763173fa43eabc25f065d3d9",
+    "url": "/more-beer-less-tears-form/static/media/fairytaleale.0588f679.png"
+  },
+  {
+    "revision": "c5e44136dbc0f0e870410c613d5597e4",
+    "url": "/more-beer-less-tears-form/static/media/foobar_logo.c5e44136.svg"
+  },
+  {
+    "revision": "d5e3677b0b275079d92f7f51d428c602",
+    "url": "/more-beer-less-tears-form/static/media/githop.d5e3677b.png"
+  },
+  {
+    "revision": "d313a8e9b617eb162da3b81ac5ef5d0f",
+    "url": "/more-beer-less-tears-form/static/media/hefe.d313a8e9.png"
+  },
+  {
+    "revision": "8c9ce1fdb6c7329f8fa821de89ce5a30",
+    "url": "/more-beer-less-tears-form/static/media/hollaback.8c9ce1fd.png"
+  },
+  {
+    "revision": "8c9ce1fdb6c7329f8fa821de89ce5a30",
+    "url": "/more-beer-less-tears-form/static/media/hollabacklager.8c9ce1fd.png"
+  },
+  {
+    "revision": "7eaa6d0987fff977cb224369d7fc7017",
+    "url": "/more-beer-less-tears-form/static/media/hoppilyeverafter.7eaa6d09.png"
+  },
+  {
+    "revision": "805e608ba76428b15ae18974d05c27a2",
+    "url": "/more-beer-less-tears-form/static/media/mastercard.805e608b.svg"
+  },
+  {
+    "revision": "5f65afd9dcf8caa7bd67c9db6180b96e",
+    "url": "/more-beer-less-tears-form/static/media/mobpayicon.5f65afd9.png"
+  },
+  {
+    "revision": "8198cc2ade4dc655153327c0f92634c0",
+    "url": "/more-beer-less-tears-form/static/media/mowintime.8198cc2a.png"
+  },
+  {
+    "revision": "d2380875c1e837ebf7894c6c13be9e31",
+    "url": "/more-beer-less-tears-form/static/media/row26.d2380875.png"
+  },
+  {
+    "revision": "25b533b102a6fabab287cebdb9648d52",
+    "url": "/more-beer-less-tears-form/static/media/ruinedchildhood.25b533b1.png"
+  },
+  {
+    "revision": "f6fe959557f0a947189612fd00844f3b",
+    "url": "/more-beer-less-tears-form/static/media/sleighride.f6fe9595.png"
+  },
+  {
+    "revision": "e0499a3b2d6a65a8b5484e6f8d7bf17d",
+    "url": "/more-beer-less-tears-form/static/media/steampunk.e0499a3b.png"
+  },
+  {
+    "revision": "af4a59560316506fe509cc6e35565694",
+    "url": "/more-beer-less-tears-form/static/media/visa.af4a5956.svg"
+  }
+]);
